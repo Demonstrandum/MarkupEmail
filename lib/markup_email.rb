97d91d4ef2934638d5bc051d8c30ae5ec8f3bce8
Demonstrandum <knutsen@jetspace.co>
2017-07-17 20:29:54 +0000

    Give title if the title is empty

--- lib/markup_email.rb
+++ lib/markup_email.rb
@@ -6,10 +6,11 @@ module MarkupEmail
   class Convert
     attr_reader :content
 
-    def initialize file, title, sanitize=false
+    def initialize file, title=String.new, sanitize=false
       @file = file
       body = Render.new(@file, sanitize).body
       metadata = HTMLify.new(body)
+      title = "#{File.basename(file).split('.')[0..-2].join('.')}" if title.empty?
       metadata.title = title
       metadata.documentize
       metadata.meta
@@ -22,6 +23,7 @@ module MarkupEmail
       new_file = "#{ext_split[0, ext_split.length - 1][0]}-email.#{ext_split[-1]}.html" if name.empty?
 
       File.open("#{File.dirname(@file)}/#{new_file}", 'w') { |file| file.write @content }
+      new_file
     end
 
   end
